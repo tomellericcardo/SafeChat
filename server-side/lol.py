7c135987d7c2a8c446f3a80642b53549d8f7fd91
Riccardo Tomelleri <riki2398@gmail.com>
2017-01-30 19:28:17 +0100

    create tabelle utente e sessione

--- server-side/lol.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('database.db')
-
-c = conn.cursor()
-c.execute('''   CREATE TABLE utente
-                (username text primary key, password text not null) ''')
-c.commit()
-conn.close()

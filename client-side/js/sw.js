c0a6b0f95f9e07e755bfaa449f4b7cc255eea8b6
Riccardo Tomelleri <riki2398@gmail.com>
2017-05-11 21:33:33 +0200

    tentativo di notifica in background #2

--- client-side/js/sw.js
+++ client-side/js/sw.js
@@ -7,9 +7,9 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(clients.matchAll({
-        type: "window"
+    event.notification.close();
+    event.waitUntil(clients.matchAll({
+        type: 'window'
     }).then(function(clientList) {
         for (var i = 0; i < clientList.length; i++) {
             var client = clientList[i];
@@ -19,3 +19,32 @@ self.addEventListener('notificationclick', function(event) {
         }
     }));
 });
+
+/*
+self.addEventListener('periodicSync', function(event) {
+    event.waitUntil(function() {
+        var richiesta = new FormData();
+        richiesta.append('json', event.tag);
+        fetch('/leggi_notifiche', {
+            method: 'POST',
+            body: richiesta
+        }).then(function(risposta) {
+            return risposta.json();
+        }).then(function(risposta) {
+            if (risposta.n_notifiche != 0) {
+                if (risposta.n_notifiche == 1) {
+                    var testo_notifica = 'Hai un nuovo messaggio';
+                } else {
+                    var testo_notifica = 'Hai ' + risposta.n_notifiche + ' nuovi messaggi';
+                }
+                self.registration.showNotification('SafeChat', {
+                    body: testo_notifica,
+                    icon: '/img/icona128.png',
+                    vibrate: [200, 100, 200],
+                    tag: 'sf'
+                });
+            }
+        });
+    });
+});
+*/
